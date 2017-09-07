3470f76fd9a5a00a539491253cb394d45e26a0ae
TalAizikov <31697771+TalAizikov@users.noreply.github.com>
2017-09-06 21:33:59 -0400

    Create Waving_Snowman.js

---
+++ Waving_Snowman.js
@@ -0,0 +1,8 @@
+rect(0, 350, 400, 80); //floor
+
+ellipse(200, 300, 150, 150); //bottom circle
+ellipse(200, 200, 100, 100); //middle circle
+ellipse(200, 120, 75, 75); // head
+
+line (170, 200, 50, 100); // Waving left hand
+line (230, 200, 350, 100); // Waving left hand
