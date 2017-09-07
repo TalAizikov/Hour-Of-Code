7873affa082db48b82e940990d570a25cdd6e33b
TalAizikov <31697771+TalAizikov@users.noreply.github.com>
2017-09-06 21:41:17 -0400

    Create Project-Super_snowman.js

---
+++ Project-Super_snowman.js
@@ -0,0 +1,27 @@
+background(50, 150, 255);
+//ground
+fill(70, 70, 70);
+rect(0, 330, 400, 100);  
+// simple snowman
+fill (255, 255, 255);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
+
+//hat
+strokeWeight(15);
+line(159, 100, 238, 100);
+fill(0, 0, 0);
+rect(173, 29, 50, 68);
+
+//eyes + nose
+ellipse(184, 115, 1, 1);
+ellipse(212, 115, 1, 1);
+stroke(207, 117, 0);
+ellipse(198, 133, 1, 1);
+
+//hands
+stroke(97, 44, 0);
+line (162, 188, 50, 100); // Waving left hand
+line (239, 188, 350, 100); // Waving left hand
+
