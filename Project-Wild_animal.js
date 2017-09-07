6bd4379e882eda7e54eaf1158dc45f558fff2566
TalAizikov <31697771+TalAizikov@users.noreply.github.com>
2017-09-06 21:38:59 -0400

    Create Project-Wild_animal.js

---
+++ Project-Wild_animal.js
@@ -0,0 +1,40 @@
+background(140, 201, 54);
+//ears
+fill(255, 255, 255);
+ellipse(180, 92,40,40);//left
+fill(150, 75, 0);
+ellipse(183, 97,30,30);
+fill(255, 255, 255);
+ellipse(218, 92,40,40);//right
+fill(150, 75, 0);
+ellipse(215, 97,30,30);
+
+//hands + feet
+fill(87, 44, 0);
+ellipse(136,156, 30, 30);
+ellipse(266,156, 30, 30);
+ellipse(158,261, 40, 30);
+ellipse(237,259, 42, 30);
+
+//body
+noStroke();
+fill(255, 255, 255);
+ellipse(200, 196, 142, 129);
+
+//face
+    //head
+    ellipse(200, 116, 64, 55);
+        //EYES
+        stroke(0, 0, 0);
+        fill(0, 0, 0);
+        ellipse(193,110, 9, 9);//left eye
+        ellipse(211,110, 9, 9);//right eye
+        //NOSE
+        stroke(87, 44, 0);
+        fill(87, 44, 0);
+        ellipse(202,118, 13, 13);//Nose
+        //Mouth
+        fill(245, 81, 81);
+        ellipse(203,129, 20, 4);
+
+        
